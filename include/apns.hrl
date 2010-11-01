2dfa80c0e48057d0e48087a5e3e84df52c2d360a
Fernando Benavides <fernando.benavides@inakanetworks.com>
2010-11-01 13:16:32 -0300

    Little tweaks

--- include/apns.hrl
+++ include/apns.hrl
@@ -1,9 +1,9 @@
 %% @doc Connection Parameters
--record(apns_connection, {ssl_seed    = "defaultSeedString"               :: string(),
+-record(apns_connection, {ssl_seed    = "someseedstring"                  :: string(),
                           apple_host  = "gateway.sandbox.push.apple.com"  :: string(),
                           apple_port  = 2195                              :: integer(),
                           cert_file   = "priv/cert.pem"                   :: string(),
-                          timeout     = 10000                             :: non_neg_integer()
+                          timeout     = 30000                             :: non_neg_integer()
                           }).
 -record(apns_msg, {device_token :: string(),
                    alert = none :: none | string(),
