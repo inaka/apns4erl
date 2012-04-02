9e89273d953a839a3f3c6b40cb7155be00a74d27
Egor Leonenko <jagor@leonenko.info>
2012-04-02 14:46:56 +0300

    logging

--- src/apns_connection.erl
+++ src/apns_connection.erl
@@ -248,8 +248,8 @@ send_payload(Socket, MsgId, Expiry, BinToken, Payload) ->
                 BinToken/binary,
                 PayloadLength:16/big,
                 BinPayload/binary>>],
-    %%error_logger:info_msg("Sending msg ~p (expires on ~p):~s~n~p~n",
-      %%                    [MsgId, Expiry, BinPayload, Packet]),
+    error_logger:info_msg("Sending msg ~p (expires on ~p):~s~n~p~n",
+                         [MsgId, Expiry, BinPayload, Packet]),
     ssl:send(Socket, Packet).
 
 hexstr_to_bin(S) ->
